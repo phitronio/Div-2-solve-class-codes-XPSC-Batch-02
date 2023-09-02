eae2a1f3686d25c75ffd255c2047c200b263d186
Apurba Saha <diponsaha007@gmail.com>
2023-09-02 23:31:43 +0600

    Updated code

--- Round 1861/C_Solve.cpp
+++ Round 1861/C_Solve.cpp
@@ -19,7 +19,7 @@ void TEST_CASES()
     int n = s.size();
     // 0 -> not sorted
     // 1 -> sorted
-    // 2 -> maybe
+    // 2 -> maybe sorted
     vector<int>all;
     for(int i=0; i<n; i++)
     {
@@ -58,14 +58,14 @@ void TEST_CASES()
             {
                 if(all.back()==1)
                     all.push_back(2);
+                else if(all.back()==2)
+                    all.push_back(2);
                 else
-                    all.push_back(all.back());
+                    all.push_back(0);
             }
         }
         else
         {
-            assert(s[i]=='-');
-            assert(!all.empty());
             all.pop_back();
         }
     }
